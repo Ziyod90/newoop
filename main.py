94f08ad513fc5eaa192cf21a0797948c267a85e1
ziyod90 <zaripov@gmail.com>
2026-08-12 14:07:15 +0500

    udate class

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 
 class Server:
-    pass
+    def __init__(self, name, ip, os, ram, cpu):
+        self.name = name
+        self.ip = ip
+        self.os = os
+        self.ram = ram
+        self.cpu = cpu
 
+server1 = Server(input('Name Server: '), input('IP: '), input('OS: '), input('RAM: '), input('CPU: '))
+# server1 = Server(
+#     "DNS server",
+#     "127.0.0.1",
+#     "Red Hat Enterprise Linux",
+#     '32',
+#     4
+# )
 
-server1 = Server()
-server1.name = "server1"
-server1.ip = "127.0.0.1"
-server1.os = "Linux"
-server1.ram = 32
-
-
-print(server1.ip)
+print('=' * 30)
+print(f'Name: {server1.name}, \nIP: {server1.ip}, \nOS: {server1.os}, \nRAM: {server1.ram}, \nCPU: {server1.cpu}')
